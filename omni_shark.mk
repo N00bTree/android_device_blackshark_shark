27a099a5b47b5e5bc2796c9e02a427e9cc782752
chankruze <chankruze@gmail.com>
2019-08-22 16:40:10 +0530

    Initial Working Commit
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ omni_shark.mk
@@ -0,0 +1,25 @@
+# Release name
+PRODUCT_RELEASE_NAME := shark
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := shark
+PRODUCT_NAME := omni_shark
+PRODUCT_BRAND := blackshark
+PRODUCT_MODEL := SKR-H0
+PRODUCT_MANUFACTURER := blackshark
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.treble.enabled=true \
+    persist.sys.usb.config=mtp \
+    persist.service.adb.enable=1 \
+    persist.service.debuggable=1 \
+    ro.secure=1 \
+    ro.adb.secure=0 \
+    ro.allow.mock.location=0
